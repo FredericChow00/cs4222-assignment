91428095773513c5161d413d117f6669bff93c7c
Ryan Chia <ryan_chia8@hotmail.com>
2025-03-12 07:35:31 +0000

    fix rotation threashold

--- assignment_1/task3.c
+++ assignment_1/task3.c
@@ -61,47 +61,68 @@ static bool change_happened;
 static void buzz_main() {
   int val = opt_3001_sensor.value(0);
   rtimer_clock_t now = RTIMER_NOW();
-  if (val == CC26XX_SENSOR_READING_ERROR || prev == -1 || abs(val - prev) < 300) {
-    // printf("b: no %d %d %d\n", val == CC26XX_SENSOR_READING_ERROR, prev == -1, abs(val - prev) < 300);
-    if (now > stop_after) {
-      buzzer_stop();
-      if (change_happened) {
-        printf("BUZZ -> IDLE\n");
-        idle_main();
-      } else {
-        printf("BUZZ -> WAIT\n");
-        wait();
-      }
-      return;
+  if (val != CC26XX_SENSOR_READING_ERROR) {
+    if (prev != -1 && abs(val - prev) >= 300) {
+      change_happened = true;
+      printf("b: light %d -> %d\n", prev, val);
+    }
+    prev = val;
+  } else {
+    printf("light sensor not ready\n");
+  }
+
+  if (now >= stop_after) {
+    buzzer_stop();
+    if (change_happened) {
+      printf("BUZZ -> IDLE\n");
+      idle_main();
+    } else {
+      printf("BUZZ -> WAIT\n");
+      wait();
     }
   } else {
-    printf("b: light %d -> %d\n", prev, val);
-    change_happened = true;
+    SENSORS_ACTIVATE(opt_3001_sensor);
+    rtimer_set(&timer, now + RTIMER_SECOND / 4, 0, buzz_main, NULL);
   }
-  prev = val;
-  
-  SENSORS_ACTIVATE(opt_3001_sensor);
-  rtimer_set(&timer, now + RTIMER_SECOND / 4, 0, buzz_main, NULL);
 }
 
 static void wait_main() {
   int val = opt_3001_sensor.value(0);
   rtimer_clock_t now = RTIMER_NOW();
-  if (val == CC26XX_SENSOR_READING_ERROR || prev == -1 || abs(val - prev) < 300) {
-    // printf("w: no %d %d %d\n", val == CC26XX_SENSOR_READING_ERROR, prev == -1, abs(val - prev) < 300);
-    if (now > stop_after) {
-      printf("WAIT -> BUZZ\n");
-      buzz();
-      return;
+  if (val != CC26XX_SENSOR_READING_ERROR) {
+    if (prev != -1 && abs(val - prev) >= 300) {
+      change_happened = true;
+      printf("b: light %d -> %d\n", prev, val);
     }
+    prev = val;
   } else {
-    printf("w: light %d -> %d\n", prev, val);
-    change_happened = true;
+    printf("light sensor not ready\n");
   }
-  prev = val;
+
+  if (now >= stop_after) {
+    printf("WAIT -> BUZZ\n");
+    buzz();
+  } else {
+    SENSORS_ACTIVATE(opt_3001_sensor);
+    rtimer_set(&timer, now + RTIMER_SECOND / 4, 0, wait_main, NULL);
+  }
+  // int val = opt_3001_sensor.value(0);
+  // rtimer_clock_t now = RTIMER_NOW();
+  // if (val == CC26XX_SENSOR_READING_ERROR || prev == -1 || abs(val - prev) < 300) {
+  //   // printf("w: no %d %d %d\n", val == CC26XX_SENSOR_READING_ERROR, prev == -1, abs(val - prev) < 300);
+  //   if (now > stop_after) {
+  //     printf("WAIT -> BUZZ\n");
+  //     buzz();
+  //     return;
+  //   }
+  // } else {
+  //   printf("w: light %d -> %d\n", prev, val);
+  //   change_happened = true;
+  // }
+  // prev = val;
   
-  SENSORS_ACTIVATE(opt_3001_sensor);
-  rtimer_set(&timer, now + RTIMER_SECOND / 4, 0, wait_main, NULL);
+  // SENSORS_ACTIVATE(opt_3001_sensor);
+  // rtimer_set(&timer, now + RTIMER_SECOND / 4, 0, wait_main, NULL);
 }
 
 static void buzz_n_wait() {
@@ -112,20 +133,20 @@ static void buzz_n_wait() {
 
 static void buzz() {
   rtimer_clock_t now = RTIMER_NOW();
-  stop_after = RTIMER_NOW() + 2 * RTIMER_SECOND;
+  stop_after = now + 2 * RTIMER_SECOND;
   
   buzzer_start(1000);
   SENSORS_ACTIVATE(opt_3001_sensor);
-  rtimer_set(&timer, RTIMER_NOW() + RTIMER_SECOND / 4, 0, buzz_main, NULL);
+  rtimer_set(&timer, now + RTIMER_SECOND / 4, 0, buzz_main, NULL);
   
 }
 
 static void wait() {
   rtimer_clock_t now = RTIMER_NOW();
-  stop_after = RTIMER_NOW() + 4 * RTIMER_SECOND;
+  stop_after = now + 4 * RTIMER_SECOND;
   
   SENSORS_ACTIVATE(opt_3001_sensor);
-  rtimer_set(&timer, RTIMER_NOW() + RTIMER_SECOND / 4, 0, wait_main, NULL);
+  rtimer_set(&timer, now + RTIMER_SECOND / 4, 0, wait_main, NULL);
 }
 
 static void idle_main() {
@@ -141,11 +162,12 @@ static void idle_main() {
 
   int rot_z = mpu_9250_sensor.value(MPU_9250_SENSOR_TYPE_GYRO_Z);
 
-  double all = sqrt(x*x + y*y + z*z);
+  int all = (int)sqrt(x*x + y*y + z*z) - 100;
 
-  double rot_all = sqrt(rot_x*rot_x, rot_y*rot_y, rot_z*rot_z);
-  // printf("%d %d %d %f\n", x,y,x,all);
-  if (all <= 200.0 && rot_all <= 10000.0) {
+  int rot_all = (int)sqrt(rot_x*rot_x + rot_y*rot_y + rot_z*rot_z);
+  // printf("acc %d.%02d G\n", all / 100, abs(all) % 100);
+  // printf("rot %d.%02d deg/s\n", rot_all / 100, abs(rot_all) % 100);
+  if (all <= 100 && rot_all <= 20000) {
     rtimer_set(&timer, RTIMER_NOW() + RTIMER_SECOND / 4, 0, idle_main, NULL);
   } else {
     printf("IDLE -> INTERIM\n");
@@ -165,16 +187,19 @@ static void interim() {
 static void interim_main() {
   int val = opt_3001_sensor.value(0);
   rtimer_clock_t now = RTIMER_NOW();
-  if (val == CC26XX_SENSOR_READING_ERROR || prev == -1 || abs(val - prev) < 300) {
-    // printf("w: no %d %d %d\n", val == CC26XX_SENSOR_READING_ERROR, prev == -1, abs(val - prev) < 300);
+  if (val != CC26XX_SENSOR_READING_ERROR) {
+    if (prev != -1 && abs(val - prev) >= 300) {
+      printf("b: light %d -> %d\n", prev, val);
+      printf("INTERIM -> BUZZ\n");
+      buzz_n_wait();
+      return;
+    }
     prev = val;
-    SENSORS_ACTIVATE(opt_3001_sensor);
-    rtimer_set(&timer, now + RTIMER_SECOND / 4, 0, interim_main, NULL);
   } else {
-    printf("i: light %d -> %d\n", prev, val);
-    printf("INTERIM -> BUZZ\n");
-    buzz_n_wait();
+    printf("light sensor not ready\n");
   }
+  SENSORS_ACTIVATE(opt_3001_sensor);
+  rtimer_set(&timer, now + RTIMER_SECOND / 4, 0, interim_main, NULL);
 }
 
 PROCESS_THREAD(process_main, ev, data) {
